9ff5dc8cafa2ab9a46526a8608a3eb4dc9804af1
qlqlrh <dongin@pusan.ac.kr>
2024-11-19 20:49:01 +0900

    Initial commit

---
+++ src/test/java/com/example/lightning/LightningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lightning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
